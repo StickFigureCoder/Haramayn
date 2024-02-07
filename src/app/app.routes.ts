3f80bd1e792637f411fadcaf500a3a37f88ae9bb
Stick Figure Coder <stickfigurecoder@gmail.com>
2024-02-07 18:23:24 +0530

    Added Routing, imporved display of newsletter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { BlogComponent } from './blog/blog.component';
+import { SupportComponent } from './support/support.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent},
+    { path: 'shop', component: ShopComponent},
+    { path: 'blog', component: BlogComponent},
+    { path: 'support', component: SupportComponent},
+    { path: 'about', component: AboutComponent}
+];
--- src/app/newsletter/newsletter.component.ts
+++ src/app/newsletter/newsletter.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-newsletter',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './newsletter.component.html',
   styleUrl: './newsletter.component.css'
 })
