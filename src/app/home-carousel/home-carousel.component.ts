53c0c3d29b02d5cbf2c4128688cfc4590d350b97
Stick Figure Coder <stickfigurecoder@gmail.com>
2024-02-08 16:56:30 +0530

    vscode is acting wierd

---
+++ src/app/home-carousel/home-carousel.component.ts
@@ -0,0 +1,50 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnChanges } from '@angular/core';
+import { RequestsService } from '../../service/requests.service';
+
+@Component({
+  selector: 'app-home-carousel',
+  standalone: true,
+  imports: [CommonModule],
+  providers: [RequestsService],
+  templateUrl: './home-carousel.component.html',
+  styleUrl: './home-carousel.component.css'
+})
+export class HomeCarouselComponent implements OnChanges {
+  slidelist: { src: String, text: String }[] = [];
+  slideIndex: number = 1;
+
+  constructor(private request_service: RequestsService) {
+    this.slidelist = this.request_service.getSlidesList();
+  }
+
+  ngOnChanges(): void {
+    this.showSlide(this.slideIndex);
+  }
+
+  // change slide with the prev/next button
+  moveSlide(moveStep: number) {
+      this.showSlide(this.slideIndex += moveStep);
+  }
+
+  // change slide with the dots
+  currentSlide(n: number) {
+      this.showSlide(this.slideIndex = n);
+  }
+
+  showSlide(n: number) {
+      const slides = document.getElementsByClassName("slide");
+      
+      if (n > slides.length) { this.slideIndex = 1 }
+      if (n < 1) { this.slideIndex = slides.length }
+
+      // hide all slides
+      for (let i = 0; i < slides.length; i++) {
+          slides[i].classList.add('hidden');
+      }
+
+      // show the active slide
+      slides[this.slideIndex - 1].classList.remove('hidden');
+  }
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HomeCarouselComponent } from '../home-carousel/home-carousel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HomeCarouselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
