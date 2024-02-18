beb12e2f177edf25fdb472b0c8f78abe48cfad4d
Stick Figure Coder <stickfigurecoder@gmail.com>
2024-02-18 13:35:14 +0530

    About us page and Favorite page complete.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { ProductImage } from '../../service/constants.service';
+import { RequestsService } from '../../service/requests.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  about_us_image_list: ProductImage[] = [];
+
+  constructor(private request_service: RequestsService) {
+    this.request_service.getLatestArrivalList().subscribe(
+      response => {
+        this.about_us_image_list = [];
+        response.products.map(
+          item => {
+            this.about_us_image_list.push(
+              {
+                id: item.id,
+                // image: item.images[0],
+                image: "https://dummyjson.com/image/500"
+              }
+            )
+          }
+        )
+      }
+    )
+  }
 
 }
--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,12 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ProductImageAndTitleAndPrice } from '../../service/constants.service';
+import { RequestsService } from '../../service/requests.service';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
+  favorite_product_list: ProductImageAndTitleAndPrice[] = [];
 
+  constructor(private request_service: RequestsService) {
+    this.request_service.getOurproductsList().subscribe(
+      response => {
+        this.favorite_product_list = [];
+        response.products.map(
+          item => {
+            this.favorite_product_list.push(
+              {
+                id: item.id,
+                title: item.title,
+                price: item.price,
+                // image: item.images[0],
+                image: "https://dummyjson.com/image/500"
+              }
+            )
+          }
+        )
+      }
+    )
+  }
 }
