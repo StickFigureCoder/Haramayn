05b475ff863133f1b77ecbfdd9ff67deff1497a4
Stick Figure Coder <stickfigurecoder@gmail.com>
2024-02-19 13:56:32 +0530

    Clean Up

--- src/service/requests.service.ts
+++ src/service/requests.service.ts
@@ -1,38 +1,65 @@
 import { Injectable } from '@angular/core';
-import { ConstantsService, ProductListResponse, ProductImageAndTitleResponse, ProductImageResponse, ProductImageAndTitleAndPriceResponse } from './constants.service';
+import {
+  ConstantsService,
+  ProductListResponse,
+  ProductImageAndTitleResponse,
+  ProductImageResponse,
+  ProductImageAndTitleAndPriceResponse,
+} from './constants.service';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RequestsService {
-
-  constructor(private constant_service: ConstantsService, private http: HttpClient) { }
+  constructor(
+    private constant_service: ConstantsService,
+    private http: HttpClient
+  ) {}
 
   getSlidesList() {
-    let final_url = this.constant_service.PRODUCTS_URL + "?limit=10&skip=10&select=images,title";
-    return this.http.get<ProductListResponse<ProductImageAndTitleResponse>>(final_url);
+    let final_url =
+      this.constant_service.PRODUCTS_URL +
+      '?limit=10&skip=10&select=images,title';
+    return this.http.get<ProductListResponse<ProductImageAndTitleResponse>>(
+      final_url
+    );
   }
 
   getCatagoryList() {
-    let final_url = this.constant_service.PRODUCTS_URL + "?limit=3&skip=10&select=images,title";
-    return this.http.get<ProductListResponse<ProductImageAndTitleResponse>>(final_url);
+    let final_url =
+      this.constant_service.PRODUCTS_URL +
+      '?limit=3&skip=10&select=images,title';
+    return this.http.get<ProductListResponse<ProductImageAndTitleResponse>>(
+      final_url
+    );
   }
 
   getLatestArrivalList() {
-    let final_url = this.constant_service.PRODUCTS_URL + "?limit=3&skip=10&select=images";
+    let final_url =
+      this.constant_service.PRODUCTS_URL + '?limit=3&skip=10&select=images';
     return this.http.get<ProductListResponse<ProductImageResponse>>(final_url);
   }
 
   getOurproductsList() {
-    let final_url = this.constant_service.PRODUCTS_URL + "?limit=3&skip=10&select=images,title,price";    
-    return this.http.get<ProductListResponse<ProductImageAndTitleAndPriceResponse>>(final_url)
-    
+    let final_url =
+      this.constant_service.PRODUCTS_URL +
+      '?limit=3&skip=10&select=images,title,price';
+    return this.http.get<
+      ProductListResponse<ProductImageAndTitleAndPriceResponse>
+    >(final_url);
   }
 
   getShopProductsList(limit: number, skip: number) {
-    let final_url = this.constant_service.PRODUCTS_URL + "?limit=" + limit + "&skip="+ skip +"&select=images,title,price";
-    return this.http.get<ProductListResponse<ProductImageAndTitleAndPriceResponse>>(final_url);
+    let final_url =
+      this.constant_service.PRODUCTS_URL +
+      '?limit=' +
+      limit +
+      '&skip=' +
+      skip +
+      '&select=images,title,price';
+    return this.http.get<
+      ProductListResponse<ProductImageAndTitleAndPriceResponse>
+    >(final_url);
   }
-
 }
